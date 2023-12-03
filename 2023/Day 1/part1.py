186fd31da5ef2d33b476b0c19b3be945f307557a
Ajay Ratnam <ajratnam.dev@gmail.com>
2023-12-03 08:53:40 +0530

    Optimize

--- 2023/Day 1/part1.py
+++ 2023/Day 1/part1.py
@@ -3,13 +3,8 @@ with open('input.txt') as file:
 
 total_sum = 0
 
-
-def get_digits(data):
-    return list(filter(str.isdigit, data))
-
-
 for case in input_data:
-    digits = get_digits(case)
-    total_sum += int(digits[0]+digits[-1])
+    digits = list(filter(str.isdigit, case))
+    total_sum += int(digits[0] + digits[-1])
 
 print(total_sum)
